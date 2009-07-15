b77d20f76a0b468fb4799e9ae32c9fa360f612cb
cternes <cternes@7beb1754-f4bf-41df-a0fe-5293ae4fbb5d>
2009-07-15 18:06:43 +0000

    git-svn-id: https://jprowlapi.svn.sourceforge.net/svnroot/jprowlapi/trunk/jProwlAPI@8 7beb1754-f4bf-41df-a0fe-5293ae4fbb5d

---
+++ Example.java
@@ -0,0 +1,16 @@
+public class Example {
+
+	public static void main(String[] args) {
+		ProwlClient c = new ProwlClient();
+		ProwlEvent e = new DefaultProwlEvent(
+				"myKey", "application", "event",
+				"message", 0);
+		String message;
+		try {
+			message = c.pushEvent(e);
+			System.out.println(message);
+		} catch (ProwlException e1) {
+			e1.printStackTrace();
+		}
+	}
+}
