d3f2b35e78de0cade889d6070f0102b2fd14ca51
David Sauer <david@nur-mal-so-am-ran.de>
2012-04-28 13:09:30 +0200

    using settigs.xml
    * added a valid key to the settings apikey
    * added a test with a real message

--- src/test/java/net/sf/prowl/ProwlClientTest.java
+++ src/test/java/net/sf/prowl/ProwlClientTest.java
@@ -7,12 +7,21 @@ import org.junit.Test;
 public class ProwlClientTest {
 
 	@Test(expected=ProwlException.class)
-	public void testSendMessage() throws ProwlException {
+	public void useWronApikey() throws ProwlException {
 		ProwlClient client = new ProwlClient("brokenKey", "junittest");
 		ProwlEvent event = new ProwlEvent("test event", "test description");
 		client.setTimeouts(2000, 2000);
 		client.setApplication("application");
 		client.setProviderKey("providerKey");
+		client.pushEvent(event);
+	}
+
+	@Test
+	public void testSendMessage() throws ProwlException {
+		ProwlClient client = new ProwlClient(System.getProperty("prowlApikey"), "junittest");
+		ProwlEvent event = new ProwlEvent("test event", "test description");
+		client.setTimeouts(2000, 2000);
+		client.setApplication("application");
 		client.pushEvent(event);
 	}
 	
